048a86ca84ec81a5e3886c964060090b8f71edda
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-09-13 13:01:30 +0000

    Cleaning up the code
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/services/application-builder/trunk@2304 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- src/main/java/com/silverpeas/applicationbuilder/WARDescriptor.java
+++ src/main/java/com/silverpeas/applicationbuilder/WARDescriptor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2000 - 2010 Silverpeas
+ * Copyright (C) 2000 - 2009 Silverpeas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -9,7 +9,7 @@
  * As a special exception to the terms and conditions of version 3.0 of
  * the GPL, you may redistribute this Program in connection with Free/Libre
  * Open Source Software ("FLOSS") applications as described in Silverpeas's
- * FLOSS exception.  You should have recieved a copy of the text describing
+ * FLOSS exception.  You should have received a copy of the text describing
  * the FLOSS exception, and it is also available here:
  * "http://repository.silverpeas.com/legal/licensing"
  *
@@ -21,6 +21,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.silverpeas.applicationbuilder;
 
 import java.util.ArrayList;
@@ -59,8 +60,8 @@ public class WARDescriptor extends XmlDocument {
       "listener", SERVLET_ELT, "servlet-mapping", "session-config", "jsp-config" };
   private static final String[] TAGS_TO_SORT =
       { NAME_ELT, DESC_ELT, "context-param", "filter",
-          "filter-mapping", "listener", SERVLET_ELT, "servlet-mapping", "session-config",
-          "jsp-config" };
+      "filter-mapping", "listener", SERVLET_ELT, "servlet-mapping", "session-config",
+      "jsp-config" };
   private static final String[] SERVLET_TAGS = { "display-name", "servlet-name", "servlet-class",
       "init-param", "load-on-startup", "param-name", "param-value" };
 
